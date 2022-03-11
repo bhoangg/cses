ba65580ab7e4132cecfe76654d625ba902219dc1
Binh Hoang <binh.hoangtuan.hust@gmail.com>
2022-03-11 08:47:23 +0700

    add template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+//g++  (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define ar array
+#define sz size
+#define pb push_back
+
+const int mxN=1e4;
+
+int solve() {
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);   
+   
+    cout << solve();
+}
